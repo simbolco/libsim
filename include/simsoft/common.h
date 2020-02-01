42db82998097a36c606c447263112e5f9754efde
Simon Struthers <snstruthers@gmail.com>
2020-02-01 12:33:29 -0800

    [common.h] Documentation overhaul + modified ForEachFuncPtr types
    to put userdata as the last parameter.

--- include/simsoft/common.h
+++ include/simsoft/common.h
@@ -173,6 +173,51 @@ typedef uint8 ubyte;
 
 CPP_NAMESPACE_START(SimSoft)
     CPP_NAMESPACE_C_API_START /* C API */
+        /**
+         * @union Sim_Variant
+         * @headerfile common.h "simsoft/common.h"
+         * @brief Union of different C data types.
+         * 
+         * @var byte  Signed byte.
+         * @var ubyte Unsigned byte.
+         * 
+         * @var signed8  8-bit signed integral.
+         * @var signed16 16-bit signed integral.
+         * @var signed32 32-bit signed integral.
+         * @var signed64 64-bit signed integral.
+         * 
+         * @var unsigned8  8-bit unsigned integral.
+         * @var unsigned16 16-bit unsigned integral.
+         * @var unsigned32 32-bit unsigned integral.
+         * @var unsigned64 64-bit unsigned integral.
+         * 
+         * @var float32 Single-precision floating-point value.
+         * @var float64 Double-precision floating-point value.
+         * 
+         * @var signed_char      Signed char.
+         * @var signed_short     Signed short.
+         * @var signed_int       Signed int.
+         * @var signed_long      Signed long.
+         * @var signed_long_long Signed long long.
+         * 
+         * @var unsigned_char      Unsigned char.
+         * @var unsigned_short     Unsigned short.
+         * @var unsigned_int       Unsigned int.
+         * @var unsigned_long      Unsigned long.
+         * @var unsigned_long_long Unsigned long long.
+         * 
+         * @var signed_size   Signed size type.
+         * @var unsigned_size Unsigned size type.
+         * 
+         * @var string  C string.
+         * @var pointer Void pointer.
+         * 
+         * @var upointer Unsigned integer pointer type.
+         * @var spointer Signed integer pointer type.
+         * @var ptr_diff Pointer difference type.
+         * 
+         * @var time Time type.
+         */
         typedef union Sim_Variant {
             // Bytes
             sint8 byte;
@@ -230,8 +275,19 @@ CPP_NAMESPACE_START(SimSoft)
 
         /**
          * @enum Sim_ReturnCode
-         * @brief C enumeration of return values of library functions
+         * @headerfile common.n "simsoft/common.h"
+         * @brief C enumeration of return values of library functions.
          * 
+         * @var SIM_RC_SUCCESS      The function did what it was supposed to do without any issues.
+         * @var SIM_RC_FAILURE      The function did what it was supposed to do, but unsuccessfully.
+         * @var SIM_RC_ERR_NULLPTR  A nullptr was passed into the function.
+         * @var SIM_RC_ERR_OUTOFMEM Ran out of memory when attempting to allocate something.
+         * @var SIM_RC_ERR_ZERODIV  Arithmetic zero-division error.
+         * @var SIM_RC_ERR_INVALARG Invalid argument passed into function.
+         * @var SIM_RC_ERR_OUTOFBND Index argument out of bounds of container.
+         * @var SIM_RC_ERR_UNSUPRTD Unsupported operation on given platform.
+         * @var SIM_RC_ERR_NOTFOUND Item not found in container.
+         * @var SIM_RC_ERR_UNIMPLTD Unimplemented/unfinished operation.
          */
         typedef enum Sim_ReturnCode {
             ENUMERATE_RETURN_CODES
@@ -240,6 +296,16 @@ CPP_NAMESPACE_START(SimSoft)
 
 #       undef __ENUMERATE_RETURN_CODE
 
+        /**
+         * @enum Sim_DataType
+         * @headerfile common.h "simsoft/common.h"
+         * @brief C enumeration of data type info.
+         * 
+         * @var SIM_DATATYPE_OTHER    User-defined type.
+         * @var SIM_DATATYPE_INTEGRAL Signed integral type.
+         * @var SIM_DATATYPE_UNSIGNED Unsigned integral type.
+         * @var SIM_DATATYPE_FLOAT    Floating-point type.
+         */
         typedef enum Sim_DataType {
             SIM_DATATYPE_OTHER    = 0x0,
             SIM_DATATYPE_INTEGRAL = 0x1,
@@ -249,47 +315,123 @@ CPP_NAMESPACE_START(SimSoft)
 
         /**
          * @struct Sim_TypeInfo
+         * @headerfile common.h "simsoft/common.h"
          * @brief Structure used by collections to optimize sorting when using built-in C
          *        numeric types.
+         * 
+         * @property type The data type being represented; one of four Sim_DataType values.
+         * @property size The size of the type in bytes.
          */
         typedef struct Sim_TypeInfo {
             size_t type : 2;
             size_t size : (sizeof (size_t) * 8) - 2;
         } Sim_TypeInfo;
 
+        /**
+         * @struct Sim_PointerPair
+         * @headerfile common.h "simsoft/common.h"
+         * @brief Structure containing two generic pointers.
+         * 
+         * @property item1 First pointer.
+         * @property item2 Second pointer.
+         */
         typedef struct Sim_PointerPair {
             void *const item1;
             void *const item2;
         } Sim_PointerPair;
 
-        typedef int Sim_ComparisonFunction(const void* const, const void* const);
-
-        typedef bool (*Sim_PredicateFuncPtr)(
-            const void* const item1,
-            const void* const item2
-        );
-
+        /**
+         * @typedef Sim_ComparisonFunction
+         * @headerfile common.h "simsoft/common.h"
+         * @brief Function pointer for a comparison between two values.
+         * 
+         * @param[in] item1 Pointer to the first item to compare.
+         * @param[in] item2 Pointer to the second item to compare.
+         * 
+         * @return An int > 0 if item1 > item2; int < 0 if item2 > item1; 0 if item1 == item2.
+         */
         typedef int (*Sim_ComparisonFuncPtr)(
             const void* const item1,
             const void* const item2
         );
 
+        /**
+         * @typedef Sim_PredicateFuncPtr
+         * @headerfile common.h "simsoft/common.h"
+         * @brief Function pointer to a predicate function that takes two values.
+         * 
+         * @param[in] item1 Pointer to the first item to compare.
+         * @param[in] item2 Pointer to the second item to compare.
+         * 
+         * @return @c true or @c false, depending on what the predicate is used for.
+         */
+        typedef bool (*Sim_PredicateFuncPtr)(
+            const void* const item1,
+            const void* const item2
+        );
+
+        /**
+         * @typedef Sim_ForEachFuncPtr
+         * @headerfile common.h "simsoft/common.h"
+         * @brief Function pointer used when iterating over a collection of items.
+         * 
+         * @param[in,out] item     Pointer to an item in a collection.
+         * @param[in]     index    The item's index in the collection it's contained in.
+         * @param[in]     userdata User-provided callback data.
+         * 
+         * @return @c false to break out of the calling foreach loop;
+         *         @c true  to continue iterating.
+         */
         typedef bool (*Sim_ForEachFuncPtr)(
             void *const item,
-            Sim_Variant userdata,
-            const size_t index
+            const size_t index,
+            Sim_Variant userdata
         );
 
+        /**
+         * @typedef Sim_ConstForEachFuncPtr
+         * @headerfile common.h "simsoft/common.h"
+         * @brief Function pointer used when iterating over a collection of const items.
+         * 
+         * @param[in] item     Pointer to an item in a collection.
+         * @param[in] index    The item's index in the collection it's contained in.
+         * @param[in] userdata User-provided callback data.
+         * 
+         * @return @c false to break out of the calling foreach loop;
+         *         @c true  to continue iterating.
+         */
         typedef bool (*Sim_ConstForEachFuncPtr)(
             const void *const item,
-            Sim_Variant userdata,
-            const size_t index
+            const size_t index,
+            Sim_Variant userdata
         );
 
+        /**
+         * @typedef Sim_HashFuncPtr
+         * @headerfile common.h "simsoft/common.h"
+         * @brief Function pointer that hashes a value.
+         * 
+         * @param[in] item    Pointer to the item being hashed.
+         * @param[in] attempt The hashing attempt; used for double-hashing of values in hash tables.
+         * 
+         * @return A hash value for the given item.
+         */
         typedef size_t (*Sim_HashFuncPtr)(
-            const void* item
+            const void *const item,
+            const size_t attempt
         );
 
+        /**
+         * @typedef Sim_FilterFuncPtr
+         * @headerfile common.h "simsoft/common.h"
+         * @brief Function pointer used when filtering a collection of items.
+         * 
+         * @param[in] item     Pointer to the item being filtered.
+         * @param[in] userdata User-provided callback data.
+         *
+         * @return @c true  to keep the item in the given container;
+         *         @c false to remove.
+         */
         typedef bool (*Sim_FilterFuncPtr)(
             const void *const item,
             Sim_Variant userdata
