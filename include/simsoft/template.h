cd4aaeb275d0bf9ea07f5da9abecc5f04f838c66
Simon Struthers <snstruthers@gmail.com>
2020-07-26 14:39:08 -0700

    [template.h] Updated for libsim 0.2

--- include/simsoft/template.h
+++ include/simsoft/template.h
@@ -1,8 +1,8 @@
 /**
  * @file xxxxxx.h
  * @author Simon Struthers (snstruthers@gmail.com)
- * @brief Header for xxxxxx
- * @version 0.1
+ * @brief 
+ * @version 0.2
  * @date YYYY-MM-DD
  * 
  * @copyright Copyright (c) YYYY LGPLv3
@@ -12,20 +12,19 @@
 #ifndef SIMSOFT_XXXXXX_H_
 #define SIMSOFT_XXXXXX_H_
 
-#include "./common.h"
+#include "macro.h"
 
 CPP_NAMESPACE_START(SimSoft)
-    CPP_NAMESPACE_C_API_START /* C API */
+    CPP_NAMESPACE_C_API_START // start C API
 
         //
     
-    CPP_NAMESPACE_C_API_END /* end C API */
-
-#   ifdef __cplusplus /* C++ API */
+    CPP_NAMESPACE_C_API_END // end C API
+#   ifdef __cplusplus       // start C++ API
         
         //
 
-#   endif /* end C++ API */
-CPP_NAMESPACE_END(SimSoft) /* end SimSoft namespace */
+#   endif // end C++ API
+CPP_NAMESPACE_END(SimSoft)
 
-#endif /* SIMSOFT_XXXXXX_H_ */
+#endif // SIMSOFT_XXXXXX_H_
